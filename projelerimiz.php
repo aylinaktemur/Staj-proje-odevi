7fe115fcc7605526fd635d2f50ad3ce42795a37c
Aylin Aktemur <aylinaktemur550@gmail.com>
2025-08-08 03:18:15 +0300

    v0.6
    - Projelerimiz Sayfası içeriği eklendi

--- projelerimiz.php
+++ projelerimiz.php
@@ -24,40 +24,18 @@
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="project-card">
                         <div class="project-image">
-                            <img src="#" alt="#">
+                            <img src="./images/photo-1460925895917-afdab827c52f.jpeg" alt="E-Belediye">
                         </div>
                         <div class="project-content">
                             <div class="project-icon">
                                 <i class="fas fa-globe"></i>
                             </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
+                            <h5 class="project-title">E-Belediye Portalı</h5>
+                            <p class="project-description">Vatandaşların belediye hizmetlerine online erişim sağlayan kapsamlı portal sistemi. 7/24 hizmet veren modern web uygulaması.</p>
                             <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="project-card">
-                        <div class="project-image">
-                            <img src="#" alt="#">
-                        </div>
-                        <div class="project-content">
-                            <div class="project-icon">
-                                <i class="fas fa-globe"></i>
-                            </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
-                            <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
+                                <span class="feature-tag">Web Uygulaması</span>
+                                <span class="feature-tag">Online Hizmet</span>
+                                <span class="feature-tag">Mobil Uyumlu</span>
                             </div>
                         </div>
                     </div>
@@ -66,18 +44,38 @@
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="project-card">
                         <div class="project-image">
-                            <img src="#" alt="#">
+                            <img src="./images/photo-1486406146926-c627a92ad1ab.jpeg" alt="Akıllı Şehir">
+                        </div>
+                        <div class="project-content">
+                            <div class="project-icon">
+                                <i class="fas fa-city"></i>
+                            </div>
+                            <h5 class="project-title">Akıllı Şehir Uygulaması</h5>
+                            <p class="project-description">Şehir yönetimini kolaylaştıran akıllı şehir projesi. IoT teknolojileri ile entegre sistem.</p>
+                            <div class="project-features">
+                                <span class="feature-tag">IoT Teknolojisi</span>
+                                <span class="feature-tag">Sensör Ağı</span>
+                                <span class="feature-tag">Veri Analizi</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="project-card">
+                        <div class="project-image">
+                            <img src="./images/photo-1551288049-bebda4e38f71.jpeg" alt="CBS Sistemi">
                         </div>
                         <div class="project-content">
                             <div class="project-icon">
                                 <i class="fas fa-map-marked-alt"></i>
                             </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
+                            <h5 class="project-title">CBS Harita Sistemi</h5>
+                            <p class="project-description">Coğrafi bilgi sistemleri ile geliştirilen kapsamlı harita uygulaması. Şehir planlaması için kritik sistem.</p>
                             <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
+                                <span class="feature-tag">CBS Teknolojisi</span>
+                                <span class="feature-tag">Harita Analizi</span>
+                                <span class="feature-tag">3D Görselleştirme</span>
                             </div>
                         </div>
                     </div>
@@ -86,18 +84,18 @@
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="project-card">
                         <div class="project-image">
-                            <img src="#" alt="#">
+                            <img src="./images/photo-1512941937669-90a1b58e7e9c.jpeg" alt="Mobil Uygulama">
                         </div>
                         <div class="project-content">
                             <div class="project-icon">
                                 <i class="fas fa-mobile-alt"></i>
                             </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
+                            <h5 class="project-title">Mobil Belediye Uygulaması</h5>
+                            <p class="project-description">Vatandaşların mobil cihazlardan belediye hizmetlerine erişebileceği modern mobil uygulama.</p>
                             <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
+                                <span class="feature-tag">Mobil Uygulama</span>
+                                <span class="feature-tag">iOS/Android</span>
+                                <span class="feature-tag">Push Bildirim</span>
                             </div>
                         </div>
                     </div>
@@ -106,18 +104,18 @@
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="project-card">
                         <div class="project-image">
-                            <img src="#" alt="#">
+                            <img src="./images/photo-1551434678-e076c223a692.jpeg" alt="Veri Merkezi">
                         </div>
                         <div class="project-content">
                             <div class="project-icon">
                                 <i class="fas fa-database"></i>
                             </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
+                            <h5 class="project-title">Veri Merkezi Projesi</h5>
+                            <p class="project-description">Belediye verilerinin güvenli şekilde saklandığı ve yönetildiği modern veri merkezi sistemi.</p>
                             <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
+                                <span class="feature-tag">Veri Güvenliği</span>
+                                <span class="feature-tag">Yedekleme</span>
+                                <span class="feature-tag">7/24 Erişim</span>
                             </div>
                         </div>
                     </div>
@@ -126,18 +124,18 @@
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="project-card">
                         <div class="project-image">
-                            <img src="#" alt="#">
+                            <img src="./images/photo-1551288049-bebda4e38f71.jpeg" alt="Güvenlik Sistemi">
                         </div>
                         <div class="project-content">
                             <div class="project-icon">
                                 <i class="fas fa-shield-alt"></i>
                             </div>
-                            <h5 class="project-title"></h5>
-                            <p class="project-description"></p>
+                            <h5 class="project-title">Güvenlik Kamera Sistemi</h5>
+                            <p class="project-description">Şehir genelinde kurulan akıllı güvenlik kamera sistemi. 7/24 izleme ve kayıt sistemi.</p>
                             <div class="project-features">
-                                <span class="feature-tag">1</span>
-                                <span class="feature-tag">2</span>
-                                <span class="feature-tag">3</span>
+                                <span class="feature-tag">Güvenlik</span>
+                                <span class="feature-tag">7/24 İzleme</span>
+                                <span class="feature-tag">Akıllı Analiz</span>
                             </div>
                         </div>
                     </div>
